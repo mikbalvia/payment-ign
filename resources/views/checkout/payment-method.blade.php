c1fc5b0a76e438a89469edf3c3ae77a9655fd218
Muhammad Ikhsan <muhammad.ikhsan.r@gmail.com>
2020-12-31 00:23:21 +0700

    fix: change response property

--- resources/views/checkout/payment-method.blade.php
+++ resources/views/checkout/payment-method.blade.php
@@ -489,7 +489,7 @@
                         icon: "success",
                         buttons: "OK"
                     }).then((value) => {
-                        window.location = "{{url('/checkout/finish')}}/" + $("#productCode").val() + "/2-" + data.bank;
+                        window.location = "{{url('/checkout/finish')}}/" + $("#productCode").val() + "/2-" + value.bank;
                     });
                 }
             },
